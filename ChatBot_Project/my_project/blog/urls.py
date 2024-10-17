79e07c26d96793ad0772e7f7d746a953b8f877ed
Mostafa Seyedi <134061258+Mostafa-Seyedi@users.noreply.github.com>
2024-10-17 13:20:09 +0200

    Update urls.py

--- ChatBot_Project/my_project/blog/urls.py
+++ ChatBot_Project/my_project/blog/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [ 
-    # Creating url --> it takes 3 parameters : 1) url itself(empty means main of blog app)
-    #                                          2) view or file or function we want to execute <.index is the name of the function>
-    #                                          3) name of the url 
     path('', views.index, name = 'index'),
     # url's that send the data from the frontend to the backend
     path('getResponse', views.getResponse, name='getResponse')
